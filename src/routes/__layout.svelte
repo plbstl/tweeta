f558fc23739094c6e813f8fd6465c8723bec92a8
Paul Ebose <paulebose@gmail.com>
2022-01-06 14:26:24 +0100

    connect phantom wallet

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,11 +1,45 @@
 <script lang="ts">
+	import { walletAddress } from '$lib/stores'
 	import 'sanitize.css'
 	import 'sanitize.css/assets.css'
 	import 'sanitize.css/forms.css'
 	import 'sanitize.css/reduce-motion.css'
 	import 'sanitize.css/system-ui.css'
 	import 'sanitize.css/typography.css'
+	import { onMount } from 'svelte'
 	import '../app.css'
+
+	const checkIfWalletIsConnected = async () => {
+		try {
+			const { solana } = window
+
+			if (solana) {
+				if (solana.isPhantom) {
+					console.log('Phantom wallet found!')
+
+					/*
+					 * The solana object gives us a function that will allow us to connect
+					 * directly with the user's wallet!
+					 */
+					const response = await solana.connect({ onlyIfTrusted: true })
+					console.log('Connected with Public Key:', response.publicKey.toString())
+					walletAddress.set(response.publicKey.toString())
+				}
+			} else {
+				alert('Solana object not found! Get a Phantom Wallet 👻')
+			}
+		} catch (error) {
+			console.error(error)
+		}
+	}
+
+	onMount(() => {
+		const onLoad = async () => {
+			await checkIfWalletIsConnected()
+		}
+		window.addEventListener('load', onLoad)
+		return () => window.removeEventListener('load', onLoad)
+	})
 </script>
 
 <slot />
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,4 +1,21 @@
 <script lang="ts">
+	import { walletAddress } from '$lib/stores'
+
+	const connectWallet = async () => {
+		const { solana } = window
+
+		if (solana) {
+			const response = await solana.connect()
+			console.log('Connected with Public Key:', response.publicKey.toString())
+			walletAddress.set(response.publicKey.toString())
+		}
+	}
+
+	let walletAddress_value: string
+
+	walletAddress.subscribe((value) => {
+		walletAddress_value = value
+	})
 </script>
 
 <svelte:head>
@@ -11,6 +28,12 @@
 	<header class="header-container">
 		<p class="header">🐦 Tweeta</p>
 		<p class="sub-text">Create tweets in the Metaverse! ✨</p>
+
+		{#if !walletAddress_value}
+			<button class="cta-button connect-wallet-button" on:click={connectWallet}>
+				Connect to Wallet
+			</button>
+		{/if}
 	</header>
 
 	<footer class="footer-container">

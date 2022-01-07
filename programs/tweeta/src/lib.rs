bfb12aadafb27e6464de86b8c5cec483002473b8
Paul Ebose <paulebose@gmail.com>
2022-01-07 21:43:13 +0100

    read and write data on our contract

--- programs/tweeta/src/lib.rs
+++ programs/tweeta/src/lib.rs
@@ -5,10 +5,57 @@ declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
 #[program]
 pub mod tweeta {
     use super::*;
-    pub fn initialize(_ctx: Context<Initialize>) -> ProgramResult {
+    pub fn initialize(ctx: Context<Initialize>) -> ProgramResult {
+        // Get a reference to the account.
+        let base_account = &mut ctx.accounts.base_account;
+        // Initialize total_tweets.
+        base_account.total_tweets = 0;
+        Ok(())
+    }
+
+    pub fn add_tweet(ctx: Context<AddTweet>, tweet_content: String) -> ProgramResult {
+        let base_account = &mut ctx.accounts.base_account;
+        let user = &mut ctx.accounts.user;
+        // Build the struct.
+        let tweet = TweetStruct {
+            tweet_content: tweet_content.to_string(),
+            user_address: *user.to_account_info().key,
+        };
+        // Add it to the tweets_list vector.
+        base_account.tweets_list.push(tweet);
+        base_account.total_tweets += 1;
         Ok(())
     }
 }
 
+// Attach variables to the function contexts.
 #[derive(Accounts)]
-pub struct Initialize {}
+pub struct Initialize<'info> {
+    #[account(init, payer = user, space = 9000)]
+    pub base_account: Account<'info, BaseAccount>,
+    #[account(mut)]
+    pub user: Signer<'info>,
+    pub system_program: Program<'info, System>,
+}
+
+#[derive(Accounts)]
+pub struct AddTweet<'info> {
+    #[account(mut)]
+    pub base_account: Account<'info, BaseAccount>,
+    #[account(mut)]
+    pub user: Signer<'info>,
+}
+
+// Create a custom struct for tweet.
+#[derive(Debug, Clone, AnchorSerialize, AnchorDeserialize)]
+pub struct TweetStruct {
+    tweet_content: String,
+    user_address: Pubkey,
+}
+
+// Tell Solana what we want to store on this account.
+#[account]
+pub struct BaseAccount {
+    pub total_tweets: u64,
+    pub tweets_list: Vec<TweetStruct>,
+}
--- tests/tweeta.ts
+++ tests/tweeta.ts
@@ -1,17 +1,47 @@
-import * as anchor from '@project-serum/anchor';
-import { Program } from '@project-serum/anchor';
-import { Tweeta } from '../target/types/tweeta';
+import * as anchor from '@project-serum/anchor'
+import { Program } from '@project-serum/anchor'
+import { Tweeta } from '../target/types/tweeta'
 
 describe('tweeta', () => {
+	// Configure the client to use the local cluster.
+	const provider = anchor.Provider.env()
+	anchor.setProvider(provider)
 
-  // Configure the client to use the local cluster.
-  anchor.setProvider(anchor.Provider.env());
+	// Create an account keypair for our program to use.
+	const baseAccount = anchor.web3.Keypair.generate()
 
-  const program = anchor.workspace.Tweeta as Program<Tweeta>;
+	// helpful variables
+	const { SystemProgram } = anchor.web3
+	const program = anchor.workspace.Tweeta as Program<Tweeta>
 
-  it('Is initialized!', async () => {
-    // Add your test here.
-    const tx = await program.rpc.initialize({});
-    console.log("Your transaction signature", tx);
-  });
-});
+	it('Is initialized!', async () => {
+		const tx = await program.rpc.initialize({
+			accounts: {
+				baseAccount: baseAccount.publicKey,
+				user: provider.wallet.publicKey,
+				systemProgram: SystemProgram.programId,
+			},
+			signers: [baseAccount],
+		})
+		console.log('--- Your transaction signature:', tx)
+	})
+
+	it('Has fetched data from the account!', async () => {
+		const account = await program.account.baseAccount.fetch(baseAccount.publicKey)
+		console.log('--- Tweet Count:', account.totalTweets.toString())
+	})
+
+	it('Has added a tweet to the account!', async () => {
+		await program.rpc.addTweet('insert_a_fancy_tweet_here', {
+			accounts: {
+				baseAccount: baseAccount.publicKey,
+				user: provider.wallet.publicKey,
+			},
+		})
+		// Get the account to see change.
+		const account = await program.account.baseAccount.fetch(baseAccount.publicKey)
+		console.log('--- Tweet Count:', account.totalTweets.toString())
+		// Access tweets_list on the account!
+		console.log('--- Tweet List:', account.tweetsList)
+	})
+})
